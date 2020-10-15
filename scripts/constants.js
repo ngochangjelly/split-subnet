6e8004c2dd8932882bf31524792e87e4a002f74b
ngochangjelly <ngochang29397@gmail.com>
2020-10-15 15:44:29 +0700

    add each ip class detail info

---
+++ scripts/constants.js
@@ -0,0 +1,171 @@
+
+export default {
+    ipClassInfo: [
+        {
+            class: 'A', value: [
+                {
+                    networkBits: 8,
+                    subnetMask: '255.0.0.0',
+                    bitsBorrowed: 0,
+                    subnets: 1,
+                    hostsInEachSubnet: 16777214
+                },
+                {
+                    networkBits: 9,
+                    subnetMask: '255.128.0.0',
+                    bitsBorrowed: 1,
+                    subnets: 2,
+                    hostsInEachSubnet: 8388606
+                },
+                {
+                    networkBits: 10,
+                    subnetMask: '255.192.0.0',
+                    bitsBorrowed: 2,
+                    subnets: 4,
+                    hostsInEachSubnet: 4194302
+                },
+                {
+                    networkBits: 11,
+                    subnetMask: '255.224.0.0',
+                    bitsBorrowed: 3,
+                    subnets: 8,
+                    hostsInEachSubnet: 2097150
+                },
+                {
+                    networkBits: 12,
+                    subnetMask: '255.240.0.0',
+                    bitsBorrowed: 4,
+                    subnets: 16,
+                    hostsInEachSubnet: 1048574
+                },
+                {
+                    networkBits: 13,
+                    subnetMask: '255.248.0.0',
+                    bitsBorrowed: 5,
+                    subnets: 32,
+                    hostsInEachSubnet: 524286
+                }
+                ,
+                {
+                    networkBits: 14,
+                    subnetMask: '255.252.0.0',
+                    bitsBorrowed: 6,
+                    subnets: 64,
+                    hostsInEachSubnet: 262142
+                },
+                {
+                    networkBits: 15,
+                    subnetMask: '255.254.0.0',
+                    bitsBorrowed: 7,
+                    subnets: 128,
+                    hostsInEachSubnet: 131070
+                },
+                {
+                    networkBits: 16,
+                    subnetMask: '255.255.0.0',
+                    bitsBorrowed: 8,
+                    subnets: 256,
+                    hostsInEachSubnet: 65534
+                },
+                {
+                    networkBits: 17,
+                    subnetMask: '255.255.128.0',
+                    bitsBorrowed: 9,
+                    subnets: 512,
+                    hostsInEachSubnet: 32766
+                },
+                {
+                    networkBits: 18,
+                    subnetMask: '255.255.192.0',
+                    bitsBorrowed: 10,
+                    subnets: 1024,
+                    hostsInEachSubnet: 16382
+                },
+                {
+                    networkBits: 19,
+                    subnetMask: '255.255.224.0',
+                    bitsBorrowed: 11,
+                    subnets: 2048,
+                    hostsInEachSubnet: 8190
+                },
+                {
+                    networkBits: 20,
+                    subnetMask: '255.255.240.0',
+                    bitsBorrowed: 12,
+                    subnets: 4096,
+                    hostsInEachSubnet: 4094
+                },
+                {
+                    networkBits: 21,
+                    subnetMask: '255.255.248.0',
+                    bitsBorrowed: 13,
+                    subnets: 8192,
+                    hostsInEachSubnet: 2046
+                },
+                {
+                    networkBits: 22,
+                    subnetMask: '255.255.252.0',
+                    bitsBorrowed: 14,
+                    subnets: 16384,
+                    hostsInEachSubnet: 1022
+                },
+                {
+                    networkBits: 23,
+                    subnetMask: '255.255.254.0',
+                    bitsBorrowed: 15,
+                    subnets: 32768,
+                    hostsInEachSubnet: 510
+                },
+                {
+                    networkBits: 24,
+                    subnetMask: '255.255.255.0',
+                    bitsBorrowed: 16,
+                    subnets: 65536,
+                    hostsInEachSubnet: 254
+                },
+                {
+                    networkBits: 25,
+                    subnetMask: '255.255.255.128',
+                    bitsBorrowed: 17,
+                    subnets: 131072,
+                    hostsInEachSubnet: 126
+                },
+                {
+                    networkBits: 26,
+                    subnetMask: '255.255.255.192',
+                    bitsBorrowed: 18,
+                    subnets: 262144,
+                    hostsInEachSubnet: 62
+                },
+                {
+                    networkBits: 27,
+                    subnetMask: '255.255.255.224',
+                    bitsBorrowed: 19,
+                    subnets: 524288,
+                    hostsInEachSubnet: 30
+                },
+                {
+                    networkBits: 28,
+                    subnetMask: '255.255.255.240',
+                    bitsBorrowed: 20,
+                    subnets: 1048576,
+                    hostsInEachSubnet: 14
+                },
+                {
+                    networkBits: 29,
+                    subnetMask: '255.255.255.248',
+                    bitsBorrowed: 21,
+                    subnets: 2097152,
+                    hostsInEachSubnet: 6
+                },
+                {
+                    networkBits: 30,
+                    subnetMask: '255.255.255.252',
+                    bitsBorrowed: 22,
+                    subnets: 4194304,
+                    hostsInEachSubnet: 2
+                }
+            ]
+        }
+    ]
+}
--- scripts/index.js
+++ scripts/index.js
@@ -204,7 +204,7 @@ function listAllSubnets(ip, numberOfSubnets, borrowBit) {
     subnetCollection: subnetCollection
   }
 }
-function binaryToDecimal (bin) {
+function binaryToDecimal(bin) {
   return parseInt(`${bin}`, 2)
 }
 function listAllPossibleIPOfEachSubnet(ip, numberOfSubnets, borrowBit) {
@@ -219,7 +219,7 @@ function listAllPossibleIPOfEachSubnet(ip, numberOfSubnets, borrowBit) {
       li.innerHTML = `Subnet ${i} : ${subnetCollection[i].join('.')}`
     } else {
       const startAddr = [...subnetCollection[i]]
-      const endAddr = [...subnetCollection[i+1]]
+      const endAddr = [...subnetCollection[i + 1]]
       startAddr[startingIndex] = parseInt(subnetCollection[i][startingIndex]) + 1
       endAddr[2] -= 1
       endAddr[3] = binaryToDecimal(11111111) - 1
@@ -266,6 +266,366 @@ function showTable2(ip, borrowBit) {
   })
 }
 
+ipClassInfo: [
+  {
+    class: 'A', value: [
+      {
+        networkBits: 8,
+        subnetMask: '255.0.0.0',
+        bitsBorrowed: 0,
+        subnets: 1,
+        hostsInEachSubnet: 16777214
+      },
+      {
+        networkBits: 9,
+        subnetMask: '255.128.0.0',
+        bitsBorrowed: 1,
+        subnets: 2,
+        hostsInEachSubnet: 8388606
+      },
+      {
+        networkBits: 10,
+        subnetMask: '255.192.0.0',
+        bitsBorrowed: 2,
+        subnets: 4,
+        hostsInEachSubnet: 4194302
+      },
+      {
+        networkBits: 11,
+        subnetMask: '255.224.0.0',
+        bitsBorrowed: 3,
+        subnets: 8,
+        hostsInEachSubnet: 2097150
+      },
+      {
+        networkBits: 12,
+        subnetMask: '255.240.0.0',
+        bitsBorrowed: 4,
+        subnets: 16,
+        hostsInEachSubnet: 1048574
+      },
+      {
+        networkBits: 13,
+        subnetMask: '255.248.0.0',
+        bitsBorrowed: 5,
+        subnets: 32,
+        hostsInEachSubnet: 524286
+      }
+      ,
+      {
+        networkBits: 14,
+        subnetMask: '255.252.0.0',
+        bitsBorrowed: 6,
+        subnets: 64,
+        hostsInEachSubnet: 262142
+      },
+      {
+        networkBits: 15,
+        subnetMask: '255.254.0.0',
+        bitsBorrowed: 7,
+        subnets: 128,
+        hostsInEachSubnet: 131070
+      },
+      {
+        networkBits: 16,
+        subnetMask: '255.255.0.0',
+        bitsBorrowed: 8,
+        subnets: 256,
+        hostsInEachSubnet: 65534
+      },
+      {
+        networkBits: 17,
+        subnetMask: '255.255.128.0',
+        bitsBorrowed: 9,
+        subnets: 512,
+        hostsInEachSubnet: 32766
+      },
+      {
+        networkBits: 18,
+        subnetMask: '255.255.192.0',
+        bitsBorrowed: 10,
+        subnets: 1024,
+        hostsInEachSubnet: 16382
+      },
+      {
+        networkBits: 19,
+        subnetMask: '255.255.224.0',
+        bitsBorrowed: 11,
+        subnets: 2048,
+        hostsInEachSubnet: 8190
+      },
+      {
+        networkBits: 20,
+        subnetMask: '255.255.240.0',
+        bitsBorrowed: 12,
+        subnets: 4096,
+        hostsInEachSubnet: 4094
+      },
+      {
+        networkBits: 21,
+        subnetMask: '255.255.248.0',
+        bitsBorrowed: 13,
+        subnets: 8192,
+        hostsInEachSubnet: 2046
+      },
+      {
+        networkBits: 22,
+        subnetMask: '255.255.252.0',
+        bitsBorrowed: 14,
+        subnets: 16384,
+        hostsInEachSubnet: 1022
+      },
+      {
+        networkBits: 23,
+        subnetMask: '255.255.254.0',
+        bitsBorrowed: 15,
+        subnets: 32768,
+        hostsInEachSubnet: 510
+      },
+      {
+        networkBits: 24,
+        subnetMask: '255.255.255.0',
+        bitsBorrowed: 16,
+        subnets: 65536,
+        hostsInEachSubnet: 254
+      },
+      {
+        networkBits: 25,
+        subnetMask: '255.255.255.128',
+        bitsBorrowed: 17,
+        subnets: 131072,
+        hostsInEachSubnet: 126
+      },
+      {
+        networkBits: 26,
+        subnetMask: '255.255.255.192',
+        bitsBorrowed: 18,
+        subnets: 262144,
+        hostsInEachSubnet: 62
+      },
+      {
+        networkBits: 27,
+        subnetMask: '255.255.255.224',
+        bitsBorrowed: 19,
+        subnets: 524288,
+        hostsInEachSubnet: 30
+      },
+      {
+        networkBits: 28,
+        subnetMask: '255.255.255.240',
+        bitsBorrowed: 20,
+        subnets: 1048576,
+        hostsInEachSubnet: 14
+      },
+      {
+        networkBits: 29,
+        subnetMask: '255.255.255.248',
+        bitsBorrowed: 21,
+        subnets: 2097152,
+        hostsInEachSubnet: 6
+      },
+      {
+        networkBits: 30,
+        subnetMask: '255.255.255.252',
+        bitsBorrowed: 22,
+        subnets: 4194304,
+        hostsInEachSubnet: 2
+      }
+    ]
+  },
+  {
+    class: 'B', value: [
+      {
+        networkBits: 16,
+        subnetMask: '255.255.0.0',
+        bitsBorrowed: 0,
+        subnets: 0,
+        hostsInEachSubnet: 65534
+      },
+      {
+        networkBits: 17,
+        subnetMask: '255.255.128.0',
+        bitsBorrowed: 1,
+        subnets: 2,
+        hostsInEachSubnet: 32766
+      },
+      {
+        networkBits: 18,
+        subnetMask: '255.255.192.0',
+        bitsBorrowed: 2,
+        subnets: 4,
+        hostsInEachSubnet: 16382
+      },
+      {
+        networkBits: 19,
+        subnetMask: '255.255.224.0',
+        bitsBorrowed: 3,
+        subnets: 8,
+        hostsInEachSubnet: 8190
+      },
+      {
+        networkBits: 20,
+        subnetMask: '255.255.240.0',
+        bitsBorrowed: 4,
+        subnets: 16,
+        hostsInEachSubnet: 4094
+      },
+      {
+        networkBits: 21,
+        subnetMask: '255.255.248.0',
+        bitsBorrowed: 5,
+        subnets: 32,
+        hostsInEachSubnet: 2046
+      },
+      {
+        networkBits: 22,
+        subnetMask: '255.255.252.0',
+        bitsBorrowed: 6,
+        subnets: 64,
+        hostsInEachSubnet: 1022
+      },
+      {
+        networkBits: 23,
+        subnetMask: '255.255.254.0',
+        bitsBorrowed: 7,
+        subnets: 128,
+        hostsInEachSubnet: 510
+      },
+      {
+        networkBits: 24,
+        subnetMask: '255.255.255.0',
+        bitsBorrowed: 8,
+        subnets: 256,
+        hostsInEachSubnet: 254
+      },
+      {
+        networkBits: 25,
+        subnetMask: '255.255.255.128',
+        bitsBorrowed: 9,
+        subnets: 512,
+        hostsInEachSubnet: 126
+      },
+      {
+        networkBits: 26,
+        subnetMask: '255.255.255.192',
+        bitsBorrowed: 10,
+        subnets: 1024,
+        hostsInEachSubnet: 62
+      },
+      {
+        networkBits: 27,
+        subnetMask: '255.255.255.224',
+        bitsBorrowed: 11,
+        subnets: 2048,
+        hostsInEachSubnet: 30
+      },
+      {
+        networkBits: 28,
+        subnetMask: '255.255.255.240',
+        bitsBorrowed: 12,
+        subnets: 4096,
+        hostsInEachSubnet: 14
+      },
+      {
+        networkBits: 29,
+        subnetMask: '255.255.255.248',
+        bitsBorrowed: 13,
+        subnets: 8192,
+        hostsInEachSubnet: 6
+      },
+      {
+        networkBits: 30,
+        subnetMask: '255.255.255.252',
+        bitsBorrowed: 14,
+        subnets: 16384,
+        hostsInEachSubnet: 2
+      }
+    ]
+  },
+  {
+    class: 'C', value: [
+      {
+        networkBits: 24,
+        subnetMask: '255.255.255.0',
+        bitsBorrowed: 0,
+        subnets: 1,
+        hostsInEachSubnet: 254
+      },
+      {
+        networkBits: 25,
+        subnetMask: '255.255.255.128',
+        bitsBorrowed: 2,
+        subnets: 512,
+        hostsInEachSubnet: 126
+      },
+      {
+        networkBits: 26,
+        subnetMask: '255.255.255.192',
+        bitsBorrowed: 2,
+        subnets: 4,
+        hostsInEachSubnet: 62
+      },
+      {
+        networkBits: 27,
+        subnetMask: '255.255.255.224',
+        bitsBorrowed: 3,
+        subnets: 8,
+        hostsInEachSubnet: 30
+      },
+      {
+        networkBits: 28,
+        subnetMask: '255.255.255.240',
+        bitsBorrowed: 4,
+        subnets: 16,
+        hostsInEachSubnet: 14
+      },
+      {
+        networkBits: 29,
+        subnetMask: '255.255.255.248',
+        bitsBorrowed: 5,
+        subnets: 32,
+        hostsInEachSubnet: 6
+      },
+      {
+        networkBits: 30,
+        subnetMask: '255.255.255.252',
+        bitsBorrowed: 6,
+        subnets: 64,
+        hostsInEachSubnet: 2
+      }
+    ]
+  }
+]
+function showTable3(ip, mask) {
+  document.getElementById('table3').style.display = 'block'
+  tableDataTds = document.querySelectorAll('#table3 td')
+  numberOfSubnets = getNumberOfChildSubnetsWhenBorrowFromNet(mask).value
+  console.log("numberOfSubnets", numberOfSubnets)
+  const tableData = [
+    {
+      index: 0, content: ip => getClassFromIPAdress(ip)
+    },
+    {
+      index: 2, content: (ip, numberOfSubnets, mask) => listAllSubnets(ip, numberOfSubnets, mask)
+    }
+  ]
+  Array.from(tableDataTds).forEachAsyncParallel(async (node, index) => {
+    if (index === 1) {
+      node.innerHTML = await tableData[index].content(mask).asString
+      return
+    }
+    else if (index === 2) {
+      node.innerHTML = await tableData[index].content(ip, numberOfSubnets, mask).content
+      return
+    } else if (index === 3) {
+      node.innerHTML = await tableData[index].content(ip, numberOfSubnets, mask)
+      return
+    } else if (index === 0) {
+      node.innerHTML = await tableData[index].content(ip)
+    }
+  })
+}
+
 function validateForm1() {
   var ip = document.forms["myForm1"]["ip"].value.split('/')[0];
   var borrowedBits = document.forms["myForm1"]["borrowBit"].value;
@@ -278,6 +638,21 @@ function validateForm1() {
     return false;
   }
   showTable2(ip, borrowedBits)
+
+}
+function validateForm2() {
+  var ip = document.forms["myForm2"]["ip"].value.split('/')[0];
+  console.log("ip", ip)
+  var mask = document.forms["myForm2"]["mask"].value;
+  if (ip == "") {
+    alert("Fill in IP address");
+    return false;
+  }
+  if (mask == "") {
+    alert("Fill in mask number");
+    return false;
+  }
+  showTable3(ip, mask)
 
 }
 
